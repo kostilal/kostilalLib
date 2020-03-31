2aa706eafafe8639b19414b19b556014f712a4cc
Ilya Kostyukevich <kostilal@gmail.com>
2020-03-31 12:03:49 +0300

    Initial project setup

---
+++ kostilalLibExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kostilalLibExample
+//
+//  Created by Ilya Kostyukevich on 3/31/20.
+//  Copyright © 2020 Ilya Kostyukevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
