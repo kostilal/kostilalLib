9849eae0ed62e197e588ee3282d2221063263b2e
Ilya Kostyukevich <kostilal@gmail.com>
2020-03-31 11:59:01 +0300

    Initial Commit

---
+++ kostilalLib/kostilalLib.h
@@ -0,0 +1,19 @@
+//
+//  kostilalLib.h
+//  kostilalLib
+//
+//  Created by Ilya Kostyukevich on 3/31/20.
+//  Copyright © 2020 Ilya Kostyukevich. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for kostilalLib.
+FOUNDATION_EXPORT double kostilalLibVersionNumber;
+
+//! Project version string for kostilalLib.
+FOUNDATION_EXPORT const unsigned char kostilalLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <kostilalLib/PublicHeader.h>
+
+
