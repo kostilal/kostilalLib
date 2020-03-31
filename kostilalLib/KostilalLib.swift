eae8998972948b1a70501af123d885785c6a86b7
Ilya Kostyukevich <kostilal@gmail.com>
2020-03-31 12:10:59 +0300

    Added test cases

---
+++ kostilalLib/KostilalLib.swift
@@ -0,0 +1,21 @@
+//
+//  KostilalLib.swift
+//  kostilalLib
+//
+//  Created by Ilya Kostyukevich on 3/31/20.
+//  Copyright © 2020 Ilya Kostyukevich. All rights reserved.
+//
+
+import Foundation
+
+public final class KostilalLib {
+    let name = "KostilalLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
--- kostilalLibTests/kostilalLibTests.swift
+++ kostilalLibTests/kostilalLibTests.swift
@@ -10,25 +10,18 @@ import XCTest
 @testable import kostilalLib
 
 class kostilalLibTests: XCTestCase {
-
+    
+    var kostilalLib: KostilalLib!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        kostilalLib = KostilalLib()
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testAdd() {
+        XCTAssertEqual(kostilalLib.add(a: 1, b: 1), 2)
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testSub() {
+        XCTAssertEqual(kostilalLib.sub(a: 2, b: 1), 1)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
